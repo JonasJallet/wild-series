d696c447c7d4cf7d5e82daabedd41f1529ffe1a5
JonasJallet <jonas.jallet@gmail.com>
2022-12-06 12:03:45 +0100

    quest 11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
+use App\Repository\EpisodeRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
@@ -21,9 +23,10 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/show/{id<^[0-9]+$>}', name: 'show')]
-    public function show(int $id, ProgramRepository $programRepository): Response
+    public function show(int $id, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $id]);
+        $seasons = $seasonRepository->findBy(['program' => $program]);
         // same as $program = $programRepository->find($id);
 
         if (!$program) {
@@ -33,6 +36,36 @@ class ProgramController extends AbstractController
         }
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+
+    #[Route('/{programId<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}', name: 'season_show')]
+    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository, EpisodeRepository $episodeRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $programId]);
+        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+        $episodes = $episodeRepository->findBy(['season' => $season]);
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episodes' => $episodes
+        ]);
+    }
+
+    #[Route('/{programId<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}/episode/{episodeId<^[0-9]+$>}', name: 'episode_show')]
+    public function showEpisode(int $programId, int $seasonId, int $episodeId, ProgramRepository $programRepository, SeasonRepository $seasonRepository, EpisodeRepository $episodeRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $programId]);
+        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+        $episode = $episodeRepository->findOneBy(['id' => $episodeId]);
+
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode
         ]);
     }
 }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -15,6 +15,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program->setTitle('Breaking Bad');
         $program->setSynopsis('Walter White, 50 ans commence à faire du crystal meth');
         $program->setPoster('https://m.media-amazon.com/images/M/MV5BMTczMTY0MTMzOV5BMl5BanBnXkFtZTcwNDQxMTk4Nw@@._V1_.jpg');
+        $program->setCountry('États-Unis');
+        $program->setYear(2008);
         $program->setCategory($this->getReference('category_Drame'));
         $manager->persist($program);
 
@@ -22,6 +24,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program2->setTitle('Black Mirror');
         $program2->setSynopsis('Sous un angle noir et souvent satirique, la série envisage un futur proche, voire immédiat.');
         $program2->setPoster('https://m.media-amazon.com/images/M/MV5BYTM3YWVhMDMtNjczMy00NGEyLWJhZDctYjNhMTRkNDE0ZTI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg');
+        $program2->setCountry('Grande-Bretagne');
+        $program2->setYear(2011);
         $program2->setCategory($this->getReference('category_Science-Fiction'));
         $manager->persist($program2);
 
@@ -29,6 +33,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program3->setTitle('The Leftovers');
         $program3->setSynopsis('Du jour au lendemain, un 14 octobre en apparence ordinaire, 2% de la population disparaît mystérieusement de la surface de la terre.');
         $program3->setPoster('https://m.media-amazon.com/images/M/MV5BNTE3MDc1MjY4NV5BMl5BanBnXkFtZTgwMDg4MjQ4MTE@._V1_FMjpg_UX1000_.jpg');
+        $program3->setCountry('États-Unis');
+        $program3->setYear(2014);
         $program3->setCategory($this->getReference('category_Drame'));
         $manager->persist($program3);
 
@@ -36,6 +42,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program4->setTitle('Fargo');
         $program4->setSynopsis('Fargo est une série d\'anthologie basée sur le film éponyme de 1996, écrit et réalisé par les frères Coen.');
         $program4->setPoster('https://m.media-amazon.com/images/M/MV5BNDJiZDgyZjctYmRjMS00ZjdkLTkwMTEtNGU1NDg3NDQ0Yzk1XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg');
+        $program4->setCountry('États-Unis');
+        $program4->setYear(2014);
         $program4->setCategory($this->getReference('category_Comedie'));
         $manager->persist($program4);
 
@@ -43,6 +51,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program5->setTitle('The Boys');
         $program5->setSynopsis('Dans un monde fictif où les super-héros se sont laissés corrompre par la célébrité et la gloire et ont peu à peu révélé la part sombre de leur personnalité.');
         $program5->setPoster('https://m.media-amazon.com/images/M/MV5BOTEyNDJhMDAtY2U5ZS00OTMzLTkwODktMjU3MjFkZWVlMGYyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_FMjpg_UX1000_.jpg');
+        $program5->setCountry('États-Unis');
+        $program5->setYear(2019);
         $program5->setCategory($this->getReference('category_Action'));
         $manager->persist($program5);
 
